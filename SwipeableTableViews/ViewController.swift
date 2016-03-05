17ebf0ed2c113bb0cc39e22c08a02e6d16030a79
Maykel Farha <maykel@slintapps.com>
2016-03-05 14:40:05 -0600

    Initial Commit

---
+++ SwipeableTableViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeableTableViews
+//
+//  Created by Maykel Farha on 3/5/16.
+//  Copyright © 2016 Slintapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
